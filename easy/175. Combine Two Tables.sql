1116a45095517ac2c8e37b461da10b97a7f7529a
Namit Shrivastava <namit507@gmail.com>
2025-05-03 16:55:28 -0400

    added explaination

--- easy/175. Combine Two Tables.sql
+++ easy/175. Combine Two Tables.sql
@@ -1,3 +1,67 @@
 SELECT firstname,lastname,city,state
 FROM person_175 p 
 LEFT JOIN address_175 a ON p.personid = a.personid;
+
+
+
+```This query performs a LEFT JOIN between two tables, person_175 and address_175, whose structure is :
+
+                   Table "public.person_175"
+  Column   |       Type        | Collation | Nullable | Default 
+-----------+-------------------+-----------+----------+---------
+ personid  | integer           |           |          | 
+ firstname | character varying |           |          | 
+ lastname  | character varying |           |          | 
+
+leetcode_db3=# \d address_175
+                   Table "public.address_175"
+  Column   |       Type        | Collation | Nullable | Default 
+-----------+-------------------+-----------+----------+---------
+ addressid | integer           |           |          | 
+ personid  | integer           |           |          | 
+ city      | character varying |           |          | 
+ state     | character varying |           |          | 
+
+
+
+based on matching personid. 
+Here's what it does:
+
+LEFT JOIN:
+
+It includes all rows from the person_175 table.
+If a matching personid exists in the address_175 table, it includes corresponding city and state values.
+If no match exists, the city and state values are set to NULL.
+Selected Columns:
+
+firstname and lastname from the person_175 table.
+city and state from the address_175 table.
+The Result
+
+The result will contain all people from the person_175 table along with their corresponding addresses, if available.
+If a person does not have an entry in the address_175 table, city and state will be NULL.
+Example
+
+If the tables contain the following data:
+
+## Input Tables
+
+### Table: person_175
+
+| personid | firstname | lastname |
+|----------|-----------|----------|
+| 1        | John      | Doe      |
+| 2        | Jane      | Smith    |
+
+### Table: address_175
+
+| addressid | personid | city     | state |
+|-----------|----------|----------|-------|
+| 1         | 1        | New York | NY    |
+
+## Query Result
+
+| firstname | lastname | city     | state |
+|-----------|----------|----------|-------|
+| John      | Doe      | New York | NY    |
+| Jane      | Smith    | NULL     | NULL  |
